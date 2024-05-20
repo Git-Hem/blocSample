4a014350a37f8abf5f21c692a14b09d1cc4f10f8
Hemanth U M <hemanth.m@pacewisdom.com>
2024-05-20 18:32:22 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_example/features/posts/ui/post_page.dart';
+import 'package:bloc_example/features/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PostPage(),
+    );
+  }
+}
+
